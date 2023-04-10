f25390c152635d510ae291253ab53fb1ea0e4c89
hyunseo0803 <choihyunseo135@gmail.com>
2023-04-10 18:47:55 +0900

    font 추가

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StyleSheet, View } from "react-native";
 import React from "react";
 import Login from "./src/pages/login/Login";
+import * as Font from "expo-font";
 
 export default function App() {
+	Font.loadAsync({ Diary: require("./assets/fonts/EF_Diary.ttf") });
 	return (
 		<View style={styles.container}>
 			<Login />
