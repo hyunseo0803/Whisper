ab90082a1968f7af65159498b3a587e9711406df
hyunseo0803 <choihyunseo135@gmail.com>
2023-04-10 14:28:59 +0900

    import 제거

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React from "react";
 import Login from "./src/pages/login/Login";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Login />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Login />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
