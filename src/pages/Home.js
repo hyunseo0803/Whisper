dc827a8c1be76c75c4a1ff17cd7341955c53be3a
hyunseo0803 <choihyunseo135@gmail.com>
2023-04-19 16:17:05 +0900

    ✏️ logo 파일명 오타 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,36 +3,33 @@ import React from "react";
 import Calender from "../components/calender/Calender";
 import GlobalStyle from "../globalStyle/GlobalStyle";
 import { Image } from "react-native";
-import Logo from '../../assets/images/Logo.png';
+import Logo from "../../assets/images/logo.png";
 
 const Home = () => {
-
-
-
-  return (
-    <SafeAreaView style={[GlobalStyle.GWidth, styles.container]}>
-      <View style={styles.imgWrap}>
-        <Image source={Logo} style={styles.logoImg}/>
-      </View>
-			<Calender/>
-    </SafeAreaView>
-  );
-}
+	return (
+		<SafeAreaView style={[GlobalStyle.GWidth, styles.container]}>
+			<View style={styles.imgWrap}>
+				<Image source={Logo} style={styles.logoImg} />
+			</View>
+			<Calender />
+		</SafeAreaView>
+	);
+};
 
 export default Home;
 
 const styles = StyleSheet.create({
-  container: {
+	container: {
 		marginHorizontal: 20,
-  },
+	},
 
-  imgWrap:{
-    height: 18,
-    marginBottom: 40
-  },
-  logoImg:{
-    height: '100%',
-    width: '100%',
-    resizeMode: 'contain'
-  }
+	imgWrap: {
+		height: 18,
+		marginBottom: 40,
+	},
+	logoImg: {
+		height: "100%",
+		width: "100%",
+		resizeMode: "contain",
+	},
 });
