354930afc9cab9f3bdaa6636b70e835128b9d976
leemyou <limhyeon0809@gmail.com>
2023-04-20 16:17:16 +0900

    💄STYLE : TabBar 수정
     - 검색 버튼 추가
     - 배치 재조정

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -4,7 +4,7 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 
 import MoodTrackerScreen from '../pages/MoodTracker'
 import HomeScreen from '../pages/Home'
-import WriteScreen from '../pages/Write'
+import SearchScreen from '../pages/Search'
 import ListScreen from '../pages/List'
 import SettingScreen from '../pages/Setting'
 
@@ -24,32 +24,41 @@ const Tabs = ({ navigation }) => {
                   shadowRadius: 10,
                   shadowOpacity: .7,
                   borderTopWidth: 0,    // 탭 상단 선 제거
+                  backgroundColor: '#fff'
                 },
                 headerShown: false,  // 상단 안내 안보이도록
             }}
         >
-            <Tab.Screen name='Home' component={HomeScreen}
-            navigation = {navigation}
-            options={{
-              tabBarIcon: ({focused}) => (
-                <Ionicons name="home-outline" size={32} color={focused ? '#E76B5C' : 'gray'} />
-              ),
-            }}
-            />
             <Tab.Screen name='MoodTracker' component={MoodTrackerScreen}
             navigation={navigation}
             options={{
               tabBarIcon: ({focused}) => (
                 <Ionicons name="analytics-outline" size={32} color={focused ? '#E76B5C' : 'gray'} />
-              ),
-            }}
+                ),
+              }}
             />
             <Tab.Screen name='List' component={ListScreen}
               options={{
-              tabBarIcon: ({focused}) => (
-                <Ionicons name="list-outline" size={32} color={focused ? '#E76B5C' : 'gray'} />
-              ),
-            }}/>
+                tabBarIcon: ({focused}) => (
+                  <Ionicons name="list-outline" size={32} color={focused ? '#E76B5C' : 'gray'} />
+                  ),
+                }}/>
+            <Tab.Screen name='Home' component={HomeScreen}
+              navigation = {navigation}
+              options={{
+                tabBarIcon: ({focused}) => (
+                  <Ionicons name="home-outline" size={32} color={focused ? '#E76B5C' : 'gray'} />
+                ),
+              }}
+            />
+            <Tab.Screen name='Search' component={SearchScreen}
+              navigation = {navigation}
+              options={{
+                tabBarIcon: ({focused}) => (
+                  <Ionicons name="search-outline" size={32} color={focused ? '#E76B5C' : 'gray'} />
+                ),
+              }}
+            />
             <Tab.Screen name='Setting' component={SettingScreen}
               options={{
                 tabBarIcon: ({focused}) => (
