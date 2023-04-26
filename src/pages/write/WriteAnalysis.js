0774406b6fc2997c9825212c2ec3e95a62f19fc4
leemyou <limhyeon0809@gmail.com>
2023-04-26 17:32:16 +0900

    ✨FEAT : 감정분석 기능 추가
    - 구글Cloud 연동해서 감정 분석 결과 가져오는거 성공
    - 코드 분리(리펙토링)해야함.

--- src/pages/write/WriteAnalysis.js
+++ src/pages/write/WriteAnalysis.js
@@ -1,42 +1,36 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, Text, Button, Pressable, Image, Platform} from 'react-native';
+import {View, StyleSheet, Text, Button, Pressable, Image, Platform, Alert} from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { getGoogleVisionResult, moodAnalysis } from '../../util/writeDiary';
 
 const WriteAnalysis = ({navigation}) => {
 
   const [image, setImage] = useState(null);
   const [imageBase64, setImageBase64] = useState(null);
-  const [isLoding, setIsLoding] = useState(false);
+  const [analysisResult, setAnalysisResult] = useState('');
 
-  /**
-   * 카메라 접근 권한 허용 get
-   * @returns true/false
-   */
-  const getPermission = async() => {
-    if (Platform.OS !== 'web'){
-      const{
-        status,
-      } = await ImagePicker.requestCameraPermissionsAsync();
-      if( state !== 'granted'){
-        alert("감정분석을 위해서는 이미지 촬영 권한을 허용해야합니다.")
-        return false;
-      }
-      return true;
-    }
-  }
 
 
   const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
+
+    // if (Platform.OS !== 'web'){
+    //   const{
+    //     status,
+    //   } = await ImagePicker.requestCameraPermissionsAsync();
+    //   if( state !== 'granted'){
+    //     alert("감정분석을 위해서는 이미지 촬영 권한을 허용해야합니다.")
+    //     return false;
+    //   }
+    //   return true;
+    // }
     let result = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All, // 이미지 타입 지정 가능
       allowsEditing: true, // 사진 수정 허용 여부
       aspect: [4, 3], // 사진의 비율
-      quality: 1,
-      base64: true
+      quality: 0.5,
+      base64: true,
+      type: 'image',
     });
-
-    // console.log(result);
 
     if (!result.canceled) {
       setImage(result.assets[0].uri);
@@ -44,45 +38,12 @@ const WriteAnalysis = ({navigation}) => {
     }
   };
 
-
-  const submitToGoogle = async () => {
-    try {
-      setIsLoding(true)
-      let body = JSON.stringify({
-        requests: [
-          {
-            features: [
-              { type: "FACE_DETECTION", maxResults: 5 },
-              // { type: "IMAGE_PROPERTIES", maxResults: 5 },
-            ],
-            image: {
-              source: {
-                imageUri: imageBase64
-              }
-            }
-          }
-        ]
-      });
-
-      let response = await fetch(
-        "https://vision.googleapis.com/v1/images:annotate?key=" +
-          'AIzaSyDvtD7gg7HtR27QN2iuj6j180HP9TYCHpQ',
-        {
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
-          },
-          method: "POST",
-          body: body
-        }
-      );
-      let responseJson = await response.json();
-      console.log(responseJson);
-      setIsLoding(false)
-    } catch (error) {
-      console.log(error);
+  useEffect(() => {
+    getGoogleVisionResultFun = () => {
+      const result = getGoogleVisionResult(imageBase64);
+      setAnalysisResult(result)
     }
-  };
+  }, [imageBase64]);
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -90,7 +51,11 @@ const WriteAnalysis = ({navigation}) => {
       {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
       {
         imageBase64 &&
-        <Button title='분석하기!!' onPress={submitToGoogle}/>
+        <Button title='분석하기!!' onPress={() => getGoogleVisionResultFun(imageBase64)}/>
+      }
+      {
+        analysisResult !== '' &&
+        <Text>당신은 {analysisResult} 하군요!</Text>
       }
     </View>
   );
