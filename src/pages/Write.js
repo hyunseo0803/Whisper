862b54df6daa41f9645aa3bd2c323af4c3d91f5e
hyunseo0803 <choihyunseo135@gmail.com>
2023-04-28 15:55:44 +0900

    💄 일기 작성 UI 수정 중

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,24 +1,51 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, SafeAreaView } from "react-native";
 import React from "react";
+import ExpoDatePicker from "../components/ExpoDatePicker/ExpoDatePicker";
+import GlobalStyle from "../globalStyle/GlobalStyle";
 
 const Write = () => {
-  return (
-    <View style={styles.container}>
-      <Text>WritePage</Text>
-      <Button
-        title='Click Here'
-        onPress = { () => alert('Button Clicked!')}/>
-    </View>
-  );
-}
+	return (
+		<SafeAreaView
+			style={{
+				display: "flex",
+				alignItems: "center",
+				marginHorizontal: "10%",
+				marginVertical: "10%",
+				height: "90%",
+				backgroundColor: "red",
+			}}
+		>
+			<View style={styles.container}>
+				<View className="writeDiary">
+					<Text style={[styles.writeDiaryText, GlobalStyle.font_caption1]}>
+						Write Diary
+					</Text>
+				</View>
+				<ExpoDatePicker />
+			</View>
+			<View style={styles.title}>기분</View>
+		</SafeAreaView>
+	);
+};
 
 export default Write;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#8fcbbc",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	container: {
+		width: "100%",
+		justifyContent: "center",
+		backgroundColor: "yellow",
+	},
+	writeDiary: {
+		backgroundColor: "red",
+		width: "100%",
+		// height: 50,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	writeDiaryText: {
+		width: "100%",
+		textAlign: "center",
+		textAlignVertical: "center",
+	},
 });
