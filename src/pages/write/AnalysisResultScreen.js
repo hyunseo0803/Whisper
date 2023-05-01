018a29158c79c213c06b38a168a911a5b0221750
hyunseo0803 <choihyunseo135@gmail.com>
2023-05-01 14:15:23 +0900

    💄 기본 스타일 추가

--- src/pages/write/AnalysisResultScreen.js
+++ src/pages/write/AnalysisResultScreen.js
@@ -1,31 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, Text, Pressable} from 'react-native';
-import { getGoogleVisionResult, moodAnalysis } from '../../util/writeDiary';
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, Text, Pressable, SafeAreaView } from "react-native";
+import { getGoogleVisionResult, moodAnalysis } from "../../util/writeDiary";
 
-const AnalysisResultScreen = ({navigation, route}) => {
+const AnalysisResultScreen = ({ navigation, route }) => {
+	const [analysisMood, setAnalysisMood] = useState("");
 
-  const [analysisMood, setAnalysisMood] = useState('');
+	useEffect(() => {
+		const getGoogleVisionResultFun = async () => {
+			const result = await getGoogleVisionResult(route.params.imageBase64);
+			setAnalysisMood(result);
+		};
+		getGoogleVisionResultFun();
+	}, [analysisMood]);
 
-  useEffect(() => {
-    const getGoogleVisionResultFun = async() => {
-      const result = await getGoogleVisionResult(route.params.imageBase64);
-      setAnalysisMood(result)
-    } 
-    getGoogleVisionResultFun()
-  }, [analysisMood]);
-  
-    return (
-      // TODO to 현서: 감정분석 결과 및 일기 주제추천 진행해주세요.
-    <View>
-      {
-        // 분석된 감정이 공백이 아니면 분석감정 출력
-        analysisMood!=='' &&
-        <Text>{analysisMood}</Text>
-      }
-    </View>
-  );
-}
+	return (
+		// TODO to 현서: 감정분석 결과 및 일기 주제추천 진행해주세요.
+		<SafeAreaView
+			style={{
+				display: "flex",
+				alignItems: "center",
+				marginHorizontal: "10%",
+				marginVertical: "10%",
+				height: "90%",
+				backgroundColor: "red",
+			}}
+		>
+			{
+				// 분석된 감정이 공백이 아니면 분석감정 출력
+				analysisMood !== "" && <Text>{analysisMood}</Text>
+			}
+		</SafeAreaView>
+	);
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
 export default AnalysisResultScreen;
