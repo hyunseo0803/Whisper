ebbfca5888c081b234197e17277cdeea8798d86e
leemyou <limhyeon0809@gmail.com>
2023-05-04 18:50:27 +0900

    ✨FEAT : 달력 숫자 수정 함수 구현
    - 한 자리 숫자를 두 자리 숫자로 만들어주는 함수 구현 완료

--- src/util/Calender.js
+++ src/util/Calender.js
@@ -22,20 +22,34 @@ export function changeMonth(NMonth) {
   return EMonth[NMonth-1]
 }
 
-  /**
-   * 날짜 수정해주는 함수
-   * @param {int} year 
-   * @param {int} yearPlus 
-   * @param {int} month 
-   * @param {int} monthPlus 
-   * @param {int} day 
-   * @param {int} dayPlus 
-   * 
-   * @returns {date}
-   */
-  export const dayPlus = (year, yearPlus, month, monthPlus, day, dayPlus) => {
-    
-    date = new Date(`${year+yearPlus}-${month+monthPlus}-${day+dayPlus}`)
-    
-    return date
+/**
+ * 날짜 수정해주는 함수
+ * @param {int} year 
+ * @param {int} yearPlus 
+ * @param {int} month 
+ * @param {int} monthPlus 
+ * @param {int} day 
+ * @param {int} dayPlus 
+ * 
+ * @returns {date}
+ */
+export const dayPlus = (year, yearPlus, month, monthPlus, day, dayPlus) => {
+  
+  date = new Date(`${year+yearPlus}-${month+monthPlus}-${day+dayPlus}`)
+  
+  return date
+}
+
+
+/**
+ * 한자리 숫자를 두자리 숫자로 만들어주는 함수
+ * @param {int} month 
+ * @returns ex)05
+ */
+export const changeNumberTwoLength = (month) => {
+  if (month.toString().length !== 2){
+    return '0'+month
+  }else{
+    return month
   }
+}
