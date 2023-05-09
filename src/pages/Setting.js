ab3e7eda5a834a80a24722d4d688d8d675c91a32
leemyou <limhyeon0809@gmail.com>
2023-05-09 19:02:38 +0900

    💄STYLE : setting 메인 페이지 UI 완료
    - setting UI 완료

--- src/pages/Setting.js
+++ src/pages/Setting.js
@@ -1,6 +1,8 @@
-import { Pressable, StyleSheet, Text, View, Alert } from "react-native";
+import { SafeAreaView, Pressable, StyleSheet, Text, View, Alert } from "react-native";
 import React from "react";
 import { SIGNOUT } from "../util/firebase/user";
+import GlobalStyle from "../globalStyle/GlobalStyle";
+import { Ionicons, AntDesign, Octicons } from "@expo/vector-icons";
 
 const Setting = ( {navigation }) => {
 
@@ -20,39 +22,68 @@ const Setting = ( {navigation }) => {
   }
 
   return (
-    <View style={styles.container}>
-      <Text>SettingPage</Text>
-      
-      <Pressable
-      onPress={() => navigation.navigate('settingAlert')}>
-        <Text>알림시간설정</Text>
-      </Pressable>
+    <SafeAreaView style={[styles.container, GlobalStyle.safeAreaWrap]}>
+      <View style={{width:'100%', alignItems: 'center', marginTop: -30}}>
+      <Text style={[GlobalStyle.font_title1, {marginBottom: 66}]}>SETTING</Text>
+        <Pressable
+        style={[styles.buttomWrap, styles.justifyContentSB]}
+        onPress={() => navigation.navigate('settingAlert')}>
+          <View style={styles.flexDirectionRow}>
+          <Ionicons name="notifications-circle-outline" size={36} style={{marginLeft:-2}}/>
+            <Text style={[GlobalStyle.font_body, {marginLeft:7}]}>알림시간설정</Text>
+          </View>
+          <Ionicons name="chevron-forward-outline" size={25}></Ionicons>
+        </Pressable>
 
-      <Pressable
-      onPress={() => navigation.navigate('settingContact')}>
-        <Text>문의하기</Text>
-      </Pressable>
+        <Pressable
+        style={[styles.buttomWrap, styles.justifyContentSB]}
+        onPress={() => navigation.navigate('settingContact')}>
+          <View style={styles.flexDirectionRow}>
+            <AntDesign name="questioncircleo" size={30} color="black" />
+            <Text style={[GlobalStyle.font_body, {marginLeft:10}]}>문의하기</Text>
+          </View>
+          <Ionicons name="chevron-forward-outline" size={25}></Ionicons>
+        </Pressable>
 
-      <Pressable
-      onPress={() => navigation.navigate('settingContactLog')}>
-        <Text>문의 기록 보기</Text>
-      </Pressable>
+        <Pressable
+        style={[styles.buttomWrap, styles.justifyContentSB]}
+        onPress={() => navigation.navigate('settingContactLog')}>
+          <View style={styles.flexDirectionRow}>
+            <Ionicons name="list-circle-outline" size={36} style={{marginLeft:-3}}></Ionicons>
+            <Text style={[GlobalStyle.font_body, {marginLeft:7}]}>문의 기록 보기</Text>
+          </View>
+          <Ionicons name="chevron-forward-outline" size={25}></Ionicons>
+        </Pressable>
 
-      <Pressable
-      onPress={() => navigation.navigate('settingPremium')}>
-        <Text>소곤소곤 프리미엄 가입</Text>
-      </Pressable>
+        <Pressable
+        style={[styles.buttomWrap, styles.justifyContentSB]}
+        onPress={() => navigation.navigate('settingPremium')}>
+          <View style={styles.flexDirectionRow}>
+            <AntDesign name="staro" size={30} color="black" />
+            <Text style={[GlobalStyle.font_body, {marginLeft:10}]}>소곤소곤 프리미엄 가입</Text>
+          </View>
+          <Ionicons name="chevron-forward-outline" size={25}></Ionicons>
+        </Pressable>
 
-      <Pressable
-      onPress={() => {onPressBtnLogout()}}>
-        <Text>로그아웃</Text>
-      </Pressable>
+        <Pressable
+        style={[styles.buttomWrap, styles.justifyContentSB]}
+        onPress={() => {onPressBtnLogout()}}>
+          <View style={styles.flexDirectionRow}>
+            <AntDesign name="logout" size={30} color="#E76B5C" />
+            <Text style={[GlobalStyle.fontRED, GlobalStyle.font_body, {marginLeft:10}]}>로그아웃</Text>
+          </View>
+        </Pressable>
 
-      <Pressable
-      onPress={() => navigation.navigate('settingWithdrawal')}>
-        <Text>회원탈퇴하기</Text>
-      </Pressable>
-    </View>
+        <Pressable
+        style={[styles.buttomWrap, styles.justifyContentSB, {borderBottomWidth:1}]}
+        onPress={() => navigation.navigate('settingWithdrawal')}>
+          <View style={styles.flexDirectionRow}>
+            <AntDesign name="closecircleo" size={30} color="#E76B5C" />
+            <Text style={[GlobalStyle.fontRED, GlobalStyle.font_body, {marginLeft:10}]}>회원탈퇴</Text>
+          </View>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -61,8 +92,24 @@ export default Setting;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#8fcbbc",
-    alignItems: "center",
-    justifyContent: "center",
+    alignContent: 'center',
+    justifyContent: 'center'
   },
+  buttomWrap:{
+    width: '100%',
+    height: 60,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    borderTopWidth: 1,
+    borderColor: '#BDBFC4',
+  },
+  justifyContentSB:{
+    justifyContent: 'space-between'
+  },
+  flexDirectionRow:{
+    flexDirection: "row",
+    alignItems: 'center'
+  }
 });
