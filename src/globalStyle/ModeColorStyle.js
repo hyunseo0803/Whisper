351ce21fcb8434ef4d24591b243c2449c4044359
leemyou <limhyeon0809@gmail.com>
2023-05-13 17:56:27 +0900

    💄STYLE : 기본 컬러 수정

--- src/globalStyle/ModeColorStyle.js
+++ src/globalStyle/ModeColorStyle.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import { COLOR_DARK_BLUE, COLOR_DARK_PRIMARY, COLOR_DARK_QUATERNARY, COLOR_DARK_RED, COLOR_DARK_SECONDARY, COLOR_DARK_TERTIARY, COLOR_LIGHT_BLUE, COLOR_LIGHT_PRIMARY, COLOR_LIGHT_QUATERNARY, COLOR_LIGHT_RED, COLOR_LIGHT_SECONDARY, COLOR_LIGHT_TERTIARY } from "./color";
+import { COLOR_BLACK, COLOR_DARK_BLUE, COLOR_DARK_FOURTH, COLOR_DARK_PRIMARY, COLOR_DARK_QUATERNARY, COLOR_DARK_RED, COLOR_DARK_SECONDARY, COLOR_DARK_TERTIARY, COLOR_DARK_THIRD, COLOR_DARK_WHITE, COLOR_LIGHT_BLUE, COLOR_LIGHT_PRIMARY, COLOR_LIGHT_QUATERNARY, COLOR_LIGHT_RED, COLOR_LIGHT_SECONDARY, COLOR_LIGHT_TERTIARY, COLOR_LIGHT_THIRD } from "./color";
 
 const ModeColorStyle = (isDark) => StyleSheet.create({
   bg_RED: {
@@ -9,7 +9,7 @@ const ModeColorStyle = (isDark) => StyleSheet.create({
 		backgroundColor: isDark ? COLOR_DARK_BLUE : COLOR_LIGHT_BLUE
 	},
 	bg_primary: {
-		backgroundColor: isDark ? COLOR_DARK_PRIMARY : COLOR_LIGHT_PRIMARY
+		backgroundColor: isDark ? COLOR_DARK_WHITE : COLOR_BLACK
 	},
   bg_secondary :{
     backgroundColor: isDark ? COLOR_DARK_SECONDARY : COLOR_LIGHT_SECONDARY
@@ -18,7 +18,7 @@ const ModeColorStyle = (isDark) => StyleSheet.create({
     backgroundColor: isDark ? COLOR_DARK_TERTIARY : COLOR_LIGHT_TERTIARY
   },  
   bg_quaternary:{
-    backgroundColor: isDark ? COLOR_DARK_QUATERNARY : COLOR_LIGHT_QUATERNARY
+    backgroundColor: isDark ? COLOR_DARK_FOURTH : COLOR_LIGHT_QUATERNARY
   },  
 
 	font_RED: {
@@ -27,19 +27,9 @@ const ModeColorStyle = (isDark) => StyleSheet.create({
 	font_BLUE :{
 		color: isDark ? COLOR_DARK_BLUE : COLOR_LIGHT_BLUE
 	},
-	font_primary: {
-		color: isDark ? COLOR_DARK_PRIMARY : COLOR_LIGHT_PRIMARY
+	font_DEFALUT: {
+		color: isDark ? COLOR_DARK_WHITE : COLOR_BLACK
 	},
-	font_secondary: {
-		color: isDark ? COLOR_DARK_SECONDARY : COLOR_LIGHT_SECONDARY
-	},
-  font_tertiary: {
-    color : isDark ? COLOR_DARK_TERTIARY : COLOR_LIGHT_TERTIARY
-  },
-  font_quaternary:{
-    color: isDark ? COLOR_DARK_QUATERNARY : COLOR_LIGHT_QUATERNARY
-  }
-
 })
 
 export default ModeColorStyle;
