7f70ece89bd0083b38c223e78273d22d3d50df80
hyunseo0803 <choihyunseo135@gmail.com>
2023-06-08 22:10:42 +0900

    ✨ FEAT: 문의 내용 상세보기 _ 조회, 삭제 기능 구현
    💄 STYLE: 문의 내용 상세보기 UI 구현

--- src/pages/setting/settingContactDetail.js
+++ src/pages/setting/settingContactDetail.js
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet, SafeAreaView, Text, Pressable } from "react-native";
-import { readcontactDetail } from "../../util/database";
+import {
+	View,
+	StyleSheet,
+	SafeAreaView,
+	Text,
+	Pressable,
+	ScrollView,
+	Alert,
+} from "react-native";
+import { readcontactDetail, deleteContact } from "../../util/database";
+import GlobalStyle from "../../globalStyle/GlobalStyle";
+import { AntDesign } from "@expo/vector-icons";
 
 const SettingContactDetail = ({ navigation, route }) => {
 	const { params } = route;
-	const cTitle = params ? params.cTitle : null;
+	const id = params ? params.id : null;
 	const [title, setTitle] = useState("");
 	const [content, setContent] = useState("");
 	const [email, setEmail] = useState("");
@@ -12,7 +22,7 @@ const SettingContactDetail = ({ navigation, route }) => {
 	useEffect(() => {
 		const fetchDetails = async () => {
 			try {
-				const details = await readcontactDetail(cTitle);
+				const details = await readcontactDetail(id);
 				setTitle(details.title);
 				setContent(details.content);
 				setEmail(details.email);
@@ -23,7 +33,31 @@ const SettingContactDetail = ({ navigation, route }) => {
 		};
 
 		fetchDetails();
-	}, [cTitle]);
+	}, [id]);
+
+	const checkDeleteContact = () => {
+		const okhandelDeleteContact = async () => {
+			try {
+				await deleteContact(id);
+				navigation.navigate("settingContactLog");
+				console.log("지움");
+			} catch (error) {
+				console.error("Error deleting contact:", error);
+			}
+		};
+
+		Alert.alert(
+			"문의 내역을 삭제하시겠습니까?",
+			"삭제하신 내용은 복구가 불가능합니다.",
+			[
+				{ text: "취소하기" },
+				{
+					text: "삭제하기",
+					onPress: okhandelDeleteContact,
+				},
+			]
+		);
+	};
 
 	return (
 		<SafeAreaView
@@ -34,12 +68,141 @@ const SettingContactDetail = ({ navigation, route }) => {
 				marginHorizontal: 20,
 			}}
 		>
-			<View>
-				<Text>Title: {title}</Text>
-				<Text>Content: {content}</Text>
-				<Text>Email: {email}</Text>
-				<Text>Date:{date}</Text>
-			</View>
+			<ScrollView
+				contentContainerStyle={{ alignItems: "center" }}
+				showsVerticalScrollIndicator={false}
+			>
+				<View
+					style={{
+						width: "100%",
+						alignItems: "center",
+						marginBottom: 60,
+						top: 10,
+					}}
+				>
+					<Text style={GlobalStyle.font_caption1}>Inquiry history</Text>
+				</View>
+				<View
+					style={{
+						display: "flex",
+						flexDirection: "row",
+						justifyContent: "space-between",
+						alignItems: "center",
+						marginBottom: 10,
+					}}
+				>
+					<Text
+						style={[
+							{ flex: 1, textAlign: "left", marginHorizontal: 10 },
+							GlobalStyle.font_body,
+						]}
+					>
+						문의 제목
+					</Text>
+					<Text
+						style={[
+							{
+								flex: 1,
+								textAlign: "right",
+								marginHorizontal: 10,
+								color: "#5F5F5F",
+							},
+							GlobalStyle.font_caption2,
+						]}
+					>
+						{date}
+					</Text>
+				</View>
+				<View
+					style={{
+						backgroundColor: "white",
+						width: 320,
+						padding: 10,
+						justifyContent: "flex-start",
+						alignItems: "flex-start",
+
+						borderRadius: 10,
+						marginBottom: 35,
+					}}
+				>
+					<Text style={[{ textAlignVertical: "top" }, GlobalStyle.font_body]}>
+						{title}
+					</Text>
+				</View>
+				<View
+					style={{
+						display: "flex",
+						flexDirection: "row",
+						justifyContent: "space-between",
+						alignItems: "center",
+						marginBottom: 10,
+					}}
+				>
+					<Text
+						style={[{ flex: 1, marginHorizontal: 10 }, GlobalStyle.font_body]}
+					>
+						문의 내용
+					</Text>
+				</View>
+				<View
+					style={{
+						backgroundColor: "white",
+						width: 320,
+						minHeight: 340,
+						padding: 10,
+						justifyContent: "flex-start",
+						alignItems: "flex-start",
+						borderRadius: 10,
+						marginBottom: 35,
+					}}
+				>
+					<Text style={({ textAlignVertical: "top" }, GlobalStyle.font_body)}>
+						{content}
+					</Text>
+				</View>
+				<View
+					style={{
+						display: "flex",
+						flexDirection: "row",
+						alignItems: "center",
+						marginBottom: 10,
+					}}
+				>
+					<Text
+						style={[{ flex: 1, marginHorizontal: 10 }, GlobalStyle.font_body]}
+					>
+						회신 받으실 이메일
+					</Text>
+				</View>
+				<View
+					style={{
+						backgroundColor: "white",
+						width: 320,
+						padding: 10,
+						justifyContent: "flex-start",
+						alignItems: "flex-start",
+						borderRadius: 10,
+					}}
+				>
+					<Text style={[{ textAlignVertical: "top" }, GlobalStyle.font_body]}>
+						{email}
+					</Text>
+				</View>
+				<Pressable
+					onPress={checkDeleteContact}
+					style={{
+						width: "100%",
+						height: 55,
+						backgroundColor: "#E76B5C",
+						borderRadius: 15,
+						marginVertical: 40,
+						justifyContent: "center",
+						alignItems: "center",
+					}}
+				>
+					<AntDesign name="delete" size={30} color="white" />
+				</Pressable>
+			</ScrollView>
 		</SafeAreaView>
 	);
 };
